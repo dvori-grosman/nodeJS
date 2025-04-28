12450562ddcc53497a1aab1cf313f97af22b9e97
dvori-grosman <dgrs326@gmail.com>
2025-04-28 20:03:27 +0300

    change the start command1

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 import renderApi from '@api/render-api';
 
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
 renderApi.auth('rnd_btRacD5SNDorR4TCkfvj0nDnPktQ');
 renderApi.listServices({includePreviews: 'true', limit: '20'})
   .then(({ data }) => console.log(data))
   .catch(err => console.error(err));
+  console.log(`Server is running on port ${PORT}`);
+});
+
