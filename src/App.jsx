59e22d0735c9603147a9994b124fe27e34c3a639
dvori-grosman <dgrs326@gmail.com>
2026-09-09 13:13:22 +0300

    Mount accessibility toolbar site-wide

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import Pages from "@/pages/index.jsx"
 import { Toaster } from "@/components/ui/toaster"
+import AccessibilityToolbar from "@/components/AccessibilityToolbar"
 
 function App() {
   return (
     <>
       <Pages />
       <Toaster />
+      <AccessibilityToolbar />
     </>
   )
 }
