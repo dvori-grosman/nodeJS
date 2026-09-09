05df83bb9f6458cf39b870197b64d15a02a770d7
dvori-grosman <dgrs326@gmail.com>
2026-09-09 11:27:57 +0300

    Centralize site and API constants

---
+++ src/config/site.js
@@ -0,0 +1,10 @@
+export const API_BASE_URL = 'https://dance-studio-server.onrender.com/api';
+
+export const SITE_CONTACT = {
+  phone: '03-3130565',
+  email: 'b0527182273@gmail.com',
+  humanSupportHours: '10:00-16:00',
+  officeHours: 'ראשון - חמישי: 10:00-16:00'
+};
+
+export const DEFAULT_PURCHASE_URL = 'https://forms.fillout.com/t/5UoM23NsYNus';
